8361e6d784bb1d8aa5858c24bfe91ac11a9549d3
00014973 <fozilzayniddinov7777@gmail.com>
2022-01-03 13:49:01 +0500

    OverdraftEvent args class && error label

--- WinFormUI/Transactions.cs
+++ WinFormUI/Transactions.cs
@@ -20,12 +20,23 @@ namespace WinFormUI
             _customer = customer;
 
             customerText.Text = _customer.CustomerName;
+            _customer.CheckingAccount.OverdraftedEvent += CheckingAccount_OverdraftedEvent;
+        }
+
+        private void CheckingAccount_OverdraftedEvent(object sender, decimal e)
+        {
+            errorMessage.Visible = true;
         }
 
         private void makePurchaseButton_Click(object sender, EventArgs e)
         {
             bool paymentResult = _customer.CheckingAccount.MakePayment("Credit Card Purchase", amountValue.Value, _customer.SavingsAccount);
             amountValue.Value = 0;
+        }
+
+        private void errorMessage_Click(object sender, EventArgs e)
+        {
+            errorMessage.Visible = false;
         }
     }
 }
